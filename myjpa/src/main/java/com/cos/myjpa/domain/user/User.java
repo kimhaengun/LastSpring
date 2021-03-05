b1d32c53cf5dc9278323c6d140fd0eb782ffdc02
haengun <lucbo49@naver.com>
2021-03-05 09:15:38 +0900

    로그인 완료

--- myjpa/src/main/java/com/cos/myjpa/domain/user/User.java
+++ myjpa/src/main/java/com/cos/myjpa/domain/user/User.java
@@ -1,20 +1,15 @@
-package com.cos.myjpa.domain.post;
+package com.cos.myjpa.domain.user;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import com.cos.myjpa.domain.user.User;
+import com.cos.myjpa.domain.post.Post;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,20 +21,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-//테이블 자동생성
-public class Post {
+public class User {
 	@Id //pk설정
 	@GeneratedValue(strategy = GenerationType.IDENTITY)//Table,auto_increment,Sequence
 	private Long id;
-	@Column(length = 60,nullable = false)
-	private String title;
-	@Lob    //대용량 데이터
-	private String content;
-	
-	 @ManyToOne(fetch = FetchType.EAGER)  
-	 //  many는 post one은 user 관계를 맺었다//연관관계 맺는방법 (FK의 주인인곳에서 적어야한다.)
-	@JoinColumn(name = "userId")
-	private User user;
+	private String username;
+	private String password;
+	private String email;
 	
 	@CreationTimestamp //자동으로 현재 시간이 들어감.
 	private LocalDateTime createDate;
